c90e5e210ddff0d062269b2328ddfd7a0165d2f9
Alfredo Oviedo <alfredooviedo@Alfredos-MBP.lan>
2024-03-19 18:45:06 -0500

    step 2 done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,38 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
 
      ServerSocket serverSocket = null;
      Socket clientSocket = null;
 
      try {
        serverSocket = new ServerSocket(4221);
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
+       System.out.println("Listening fro connections on port 4221...");
+
+       // forever loop to keep the server running
+       while (true) {
+         serverSocket.setReuseAddress(true);
+         clientSocket = serverSocket.accept(); // Wait for connection from client.
+         System.out.println("accepted new connection from " + clientSocket);
+
+         //Prepare HTTP response
+         String httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+
+         OutputStream outputStream = clientSocket.getOutputStream();
+
+         outputStream.write(httpResponse.getBytes(StandardCharsets.UTF_8));
+         outputStream.flush();
+       }
+
+
+
+
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
