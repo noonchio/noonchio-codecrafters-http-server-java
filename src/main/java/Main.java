6a3ff8d0455f0ed1e84eaea60c58238061fe5da8
Alfredo Oviedo <alfredooviedo@Alfredos-MBP.lan>
2024-03-19 20:27:46 -0500

    step 404

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
@@ -21,8 +24,31 @@ public class Main {
          clientSocket = serverSocket.accept(); // Wait for connection from client.
          System.out.println("accepted new connection from " + clientSocket);
 
+         // Read HTTP request from the client
+         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+         String line;
+         StringBuilder requestBuilder = new StringBuilder();
+         while (!(line = in.readLine()).isEmpty()) {
+           requestBuilder.append(line + "\n");
+         }
+         String request = requestBuilder.toString();
+         System.out.println("Request from client:\n" + request);
+
+         String[] requestLines = request.split("\n");
+         System.out.println("-------> "+requestLines[0]);
+
+         String startLine = requestLines[0];
+         String[] splitStartLine = startLine.split(" ");
+         String path = splitStartLine[1];
+         String httpResponse = "";
+         if(path.equals("/")){
+           httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+         } else {
+           httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+         }
+
          //Prepare HTTP response
-         String httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+
 
          OutputStream outputStream = clientSocket.getOutputStream();
 
